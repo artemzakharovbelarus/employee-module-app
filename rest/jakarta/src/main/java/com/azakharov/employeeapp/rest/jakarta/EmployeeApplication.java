008ee7356588cba2de6680607acfea4c96b19936
Artem Zakharov <temax359x@gmail.com>
2022-06-24 11:52:00 +0200

    fix: controller module was renamed to 'controllers'

--- rest/jakarta/src/main/java/com/azakharov/employeeapp/rest/jakarta/EmployeeApplication.java
+++
@@ -1,7 +0,0 @@
-package com.azakharov.employeeapp.rest.jakarta;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("")
-public class EmployeeApplication extends Application { }
--- rest/jakarta/src/main/java/com/azakharov/employeeapp/rest/jakarta/RestJakartaModule.java
+++
@@ -1,6 +0,0 @@
-package com.azakharov.employeeapp.rest.jakarta;
-
-import com.google.inject.AbstractModule;
-
-public class RestJakartaModule extends AbstractModule {
-}
