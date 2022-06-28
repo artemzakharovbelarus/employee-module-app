f963bd2706acf3cd628f843ad391cc01b47080f6
Artem Zakharov <temax359x@gmail.com>
2022-06-28 16:17:11 +0200

    fix: env magic string were replaced with constants in hibernate module

--- repositories/hibernate/src/main/java/com/azakharov/employeeapp/repository/hibernate/HibernateModule.java
+++ repositories/hibernate/src/main/java/com/azakharov/employeeapp/repository/hibernate/HibernateModule.java
@@ -10,10 +10,24 @@ import org.hibernate.cfg.Configuration;
 
 public class HibernateModule extends AbstractModule {
 
+    private static final String JDBC_URL_KEY = "hibernate.connection.url";
+    private static final String JDBC_USERNAME_KEY = "hibernate.connection.username";
+    private static final String JDBC_PASSWORD_KEY = "hibernate.connection.password";
+
+    private static final String ENV_DATASOURCE_URL_KEY = "SPRING_DATASOURCE_URL";
+    private static final String ENV_DATASOURCE_USERNAME_KEY = "POSTGRES_USER";
+    private static final String ENV_DATASOURCE_PASSWORD_KEY = "POSTGRES_PASSWORD";
+
     @Provides
     @Singleton
     public Configuration provideHibernateConfiguration() {
-        return new Configuration().configure("hibernate.cfg.xml");
+        final var configuration = new Configuration();
+
+        configuration.setProperty(JDBC_URL_KEY, System.getenv(ENV_DATASOURCE_URL_KEY));
+        configuration.setProperty(JDBC_USERNAME_KEY, System.getenv(ENV_DATASOURCE_USERNAME_KEY));
+        configuration.setProperty(JDBC_PASSWORD_KEY, System.getenv(ENV_DATASOURCE_PASSWORD_KEY));
+
+        return configuration.configure();
     }
 
     @Provides
